3439f4f24883df746f26efcb25326d60c72a7a72
Peefy <xpf6677@163.com>
2022-05-06 20:03:10 +0800

    chore: update checksum.

--- kclvm/version/src/lib.rs
+++ kclvm/version/src/lib.rs
@@ -1,7 +1,7 @@
 // Copyright 2021 The KCL Authors. All rights reserved.
 
 pub const VERSION: &str = "0.4.1";
-pub const CHECK_SUM: &str = "cc68dfc367e70d516649638f217a53a3";
+pub const CHECK_SUM: &str = "e07ed7af0d9bd1e86a3131714e4bd20c";
 
 pub fn get_full_version() -> String {
     format!("{}-{}", VERSION, CHECK_SUM)
