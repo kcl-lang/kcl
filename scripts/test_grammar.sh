21fc89c08bde73282ba57d245f68ada308a1d1df
Peefy <xpf6677@163.com>
2022-05-07 18:03:04 +0800

    fix: gitub action KCLVM build scripts and test.

--- scripts/test_grammar.sh
+++ scripts/test_grammar.sh
@@ -4,7 +4,7 @@ topdir=$(realpath $(dirname $0)/../)
 kclvm_install_dir="$topdir/_build/dist/$os/kclvm"
 kclvm_source_dir="$topdir"
 export PATH=$kclvm_install_dir/bin:$PATH
-
+kcl -V
 # Grammar test
 cd $kclvm_source_dir/test/grammar
 kclvm -m pytest -v -n 10
