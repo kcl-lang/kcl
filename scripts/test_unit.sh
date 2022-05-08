0663a2de70fd019e1503ce7fbf84477a1f83e833
Peefy <xpf6677@163.com>
2022-05-08 15:26:18 +0800

    fix: test unit scripts and test.

--- scripts/test_unit.sh
+++ scripts/test_unit.sh
@@ -7,6 +7,9 @@ kclvm_source_dir="$topdir"
 echo PATH=$PATH:$kclvm_source_dir/_build/dist/ubuntu/kclvm/bin >> ~/.bash_profile
 source ~/.bash_profile
 
+# Install the dependency
+kclvm -m pip install nose==1.3.7
+
 # Unit test
 cd $kclvm_source_dir/test/test_units/
 kclvm -m nose
