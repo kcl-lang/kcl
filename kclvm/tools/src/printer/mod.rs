70adcef483da9d04ea9bc9e70e3e3b8000400144
Peefy <xpf6677@163.com>
2022-05-11 10:27:34 +0800

    feat: initial print tools

--- kclvm/tools/src/printer/mod.rs
+++ kclvm/tools/src/printer/mod.rs
@@ -1,4 +1,224 @@
+use indexmap::IndexMap;
+use std::collections::VecDeque;
+
+use kclvm_ast::{ast, walker::MutSelfTypedResultWalker};
+
 mod node;
 
 #[cfg(test)]
 mod tests;
+
+pub const WHITESPACE: &str = " ";
+pub const TAB: &str = "\t";
+pub const NEWLINE: &str = "\n";
+
+#[derive(Debug, Clone)]
+pub enum Indentation {
+    Indent = 0,
+    Dedent = 1,
+    Newline = 2,
+    IndentWithNewline = 3,
+    DedentWithNewline = 4,
+    Fill = 5,
+}
+
+/// Printer config
+#[derive(Debug)]
+pub struct Config {
+    pub tab_len: usize,
+    pub indent_len: usize,
+    pub use_spaces: bool,
+    pub print_comments: bool,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            tab_len: 4,
+            indent_len: 4,
+            use_spaces: true,
+            print_comments: true,
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+pub struct NoHook;
+
+impl PrinterHook for NoHook {}
+
+pub enum ASTNode<'p> {
+    Stmt(&'p ast::NodeRef<ast::Stmt>),
+    Expr(&'p ast::NodeRef<ast::Expr>),
+}
+
+pub trait PrinterHook {
+    fn pre(&self, _printer: &mut Printer<'_>, _node: ASTNode<'_>) {}
+    fn post(&self, _printer: &mut Printer<'_>, _node: ASTNode<'_>) {}
+}
+
+pub struct Printer<'p> {
+    /// Output string buffer.
+    pub out: String,
+    pub indent: usize,
+    pub cfg: Config,
+    /// Print comments,
+    pub last_ast_line: u64,
+    pub comments: VecDeque<ast::NodeRef<ast::Comment>>,
+    pub import_spec: IndexMap<String, String>,
+    pub hook: &'p (dyn PrinterHook + 'p),
+}
+
+impl Default for Printer<'_> {
+    fn default() -> Self {
+        Self {
+            out: Default::default(),
+            indent: Default::default(),
+            cfg: Default::default(),
+            last_ast_line: Default::default(),
+            comments: Default::default(),
+            import_spec: Default::default(),
+            hook: &NoHook,
+        }
+    }
+}
+
+impl<'p> Printer<'p> {
+    pub fn new(cfg: Config, hook: &'p (dyn PrinterHook + 'p)) -> Self {
+        Self {
+            out: "".to_string(),
+            indent: 0,
+            cfg,
+            last_ast_line: 0,
+            comments: VecDeque::default(),
+            import_spec: IndexMap::default(),
+            hook,
+        }
+    }
+
+    // --------------------------
+    // Write functions
+    // --------------------------
+
+    /// Write a string
+    #[inline]
+    pub fn write(&mut self, text: &str) {
+        self.print_string(text);
+    }
+
+    /// Print a word
+    #[inline]
+    pub fn writeln(&mut self, text: &str) {
+        self.print_string(text);
+        self.print_string(NEWLINE);
+    }
+
+    /// Fill a indent
+    pub fn fill(&mut self, text: &str) {
+        if self.cfg.use_spaces {
+            self.write(&format!(
+                "{}{}",
+                WHITESPACE.repeat(self.indent * self.cfg.indent_len),
+                text
+            ));
+        } else {
+            self.write(&format!("{}{}", TAB.repeat(self.indent), text));
+        }
+    }
+
+    /// Print string
+    #[inline]
+    pub fn print_string(&mut self, string: &str) {
+        self.out.push_str(string);
+    }
+
+    pub fn print_indentation(&mut self, indentation: Indentation) {
+        match indentation {
+            Indentation::Indent => self.enter(),
+            Indentation::Dedent => self.leave(),
+            Indentation::Newline => self.writeln(""),
+            Indentation::IndentWithNewline => {
+                self.enter();
+                self.writeln("")
+            }
+            Indentation::DedentWithNewline => {
+                self.leave();
+                self.writeln("");
+            }
+            Indentation::Fill => self.fill(""),
+        }
+    }
+
+    /// Print string
+    #[inline]
+    pub fn print_value<T: std::fmt::Display>(&mut self, value: T) {
+        self.write(&format!("{}", value));
+    }
+
+    /// Print string
+    #[inline]
+    pub fn print_node(&mut self, node: ASTNode<'_>) {
+        match node {
+            ASTNode::Stmt(stmt) => self.stmt(stmt),
+            ASTNode::Expr(expr) => self.expr(expr),
+        }
+    }
+
+    /// Print string
+    #[inline]
+    pub fn print_module(&mut self, module: &ast::Module) {
+        self.walk_module(module);
+        loop {
+            match self.comments.pop_front() {
+                Some(comment) => {
+                    self.writeln(&comment.node.text);
+                    self.fill("");
+                }
+                None => break,
+            }
+        }
+    }
+
+    pub fn print_ast_comments<T>(&mut self, node: &ast::NodeRef<T>) {
+        if !self.cfg.print_comments {
+            return;
+        }
+        if node.line > self.last_ast_line {
+            self.last_ast_line = node.line;
+            let mut index = None;
+            for (i, comment) in self.comments.iter().enumerate() {
+                if comment.line <= node.line {
+                    index = Some(i);
+                } else {
+                    break;
+                }
+            }
+            if let Some(index) = index {
+                let mut count = index;
+                while count > 0 {
+                    match self.comments.pop_front() {
+                        Some(comment) => {
+                            self.writeln(&comment.node.text);
+                        }
+                        None => break,
+                    }
+                    count -= 1;
+                }
+            }
+        }
+    }
+
+    // --------------------------
+    // Indent and scope functions
+    // --------------------------
+
+    /// Enter with a indent
+    pub fn enter(&mut self) {
+        self.indent += 1;
+    }
+
+    /// Leave with a dedent
+    pub fn leave(&mut self) {
+        self.indent -= 1;
+    }
+}
--- kclvm/tools/src/printer/node.rs
+++ kclvm/tools/src/printer/node.rs
@@ -1,19 +1,246 @@
-use kclvm_ast::walker::MutSelfTypedResultWalker;
+use indexmap::IndexMap;
+
 use kclvm_ast::ast;
+use kclvm_ast::token::Token;
+use kclvm_ast::walker::MutSelfTypedResultWalker;
 
-#[derive(Debug, Clone)]
-enum Indentation {
-    Indent = 0,
-    Dedent = 1,
-    Newline = 2,
-    IndentWithNewline = 3,
-    Fill = 5,
-}
+use super::Printer;
 
+const COMMA_WHITESPACE: &str = ", ";
 const INVALID_AST_MSG: &str = "Invalid AST Node";
 const TEMP_ROOT: &str = "<root>";
 
-const WHITESPACE: &str = " ";
-const TAB: &str = "\t";
-const NEWLINE: &str = "\n";
-const 
+macro_rules! interleave {
+    ($inter: stmt, $f: expr, $seq: expr) => {
+        if $seq.is_empty() {
+            return;
+        }
+        $f(&$seq[0]);
+        for s in &$seq[1..] {
+            $inter
+            $f(s);
+        }
+    };
+}
+
+impl<'p, 'ctx> MutSelfTypedResultWalker<'ctx> for Printer<'p> {
+    type Result = ();
+
+    fn walk_module(&mut self, module: &'ctx ast::Module) -> Self::Result {
+        for comment in &module.comments {
+            self.comments.push_back(comment.clone());
+        }
+        self.stmts(&module.body);
+    }
+
+    fn walk_expr_stmt(&mut self, expr_stmt: &'ctx ast::ExprStmt) -> Self::Result {
+        interleave!(
+            self.write(COMMA_WHITESPACE),
+            |expr| self.expr(expr),
+            expr_stmt.exprs
+        );
+        self.writeln("");
+    }
+
+    fn walk_unification_stmt(
+        &mut self,
+        unification_stmt: &'ctx ast::UnificationStmt,
+    ) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_type_alias_stmt(&mut self, type_alias_stmt: &'ctx ast::TypeAliasStmt) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_assign_stmt(&mut self, assign_stmt: &'ctx ast::AssignStmt) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_aug_assign_stmt(&mut self, aug_assign_stmt: &'ctx ast::AugAssignStmt) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_assert_stmt(&mut self, assert_stmt: &'ctx ast::AssertStmt) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_if_stmt(&mut self, if_stmt: &'ctx ast::IfStmt) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_import_stmt(&mut self, import_stmt: &'ctx ast::ImportStmt) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_schema_stmt(&mut self, schema_stmt: &'ctx ast::SchemaStmt) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_rule_stmt(&mut self, rule_stmt: &'ctx ast::RuleStmt) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_quant_expr(&mut self, quant_expr: &'ctx ast::QuantExpr) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_schema_attr(&mut self, schema_attr: &'ctx ast::SchemaAttr) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_if_expr(&mut self, if_expr: &'ctx ast::IfExpr) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_unary_expr(&mut self, unary_expr: &'ctx ast::UnaryExpr) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_binary_expr(&mut self, binary_expr: &'ctx ast::BinaryExpr) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_selector_expr(&mut self, selector_expr: &'ctx ast::SelectorExpr) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_call_expr(&mut self, call_expr: &'ctx ast::CallExpr) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_subscript(&mut self, subscript: &'ctx ast::Subscript) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_paren_expr(&mut self, paren_expr: &'ctx ast::ParenExpr) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_list_expr(&mut self, list_expr: &'ctx ast::ListExpr) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_list_comp(&mut self, list_comp: &'ctx ast::ListComp) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_list_if_item_expr(
+        &mut self,
+        list_if_item_expr: &'ctx ast::ListIfItemExpr,
+    ) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_starred_expr(&mut self, starred_expr: &'ctx ast::StarredExpr) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_dict_comp(&mut self, dict_comp: &'ctx ast::DictComp) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_config_if_entry_expr(
+        &mut self,
+        config_if_entry_expr: &'ctx ast::ConfigIfEntryExpr,
+    ) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_comp_clause(&mut self, comp_clause: &'ctx ast::CompClause) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_schema_expr(&mut self, schema_expr: &'ctx ast::SchemaExpr) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_config_expr(&mut self, config_expr: &'ctx ast::ConfigExpr) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_check_expr(&mut self, check_expr: &'ctx ast::CheckExpr) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_lambda_expr(&mut self, lambda_expr: &'ctx ast::LambdaExpr) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_keyword(&mut self, keyword: &'ctx ast::Keyword) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_arguments(&mut self, arguments: &'ctx ast::Arguments) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_compare(&mut self, compare: &'ctx ast::Compare) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_identifier(&mut self, identifier: &'ctx ast::Identifier) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_literal(&mut self, literal: &'ctx ast::Literal) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_number_lit(&mut self, number_lit: &'ctx ast::NumberLit) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_string_lit(&mut self, string_lit: &'ctx ast::StringLit) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_name_constant_lit(
+        &mut self,
+        name_constant_lit: &'ctx ast::NameConstantLit,
+    ) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_joined_string(&mut self, joined_string: &'ctx ast::JoinedString) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_formatted_value(&mut self, formatted_value: &'ctx ast::FormattedValue) -> Self::Result {
+        todo!()
+    }
+
+    fn walk_comment(&mut self, comment: &'ctx ast::Comment) -> Self::Result {
+        self.writeln(&comment.text);
+        self.fill("");
+    }
+}
+
+impl<'p> Printer<'p> {
+    // ------------------------------
+    // Expr and Stmt walker functions
+    // ------------------------------
+
+    pub fn expr(&mut self, expr: &ast::NodeRef<ast::Expr>) {
+        self.print_ast_comments(expr);
+        self.walk_expr(&expr.node)
+    }
+
+    pub fn stmt(&mut self, stmt: &ast::NodeRef<ast::Stmt>) {
+        self.fill("");
+        self.print_ast_comments(stmt);
+        self.walk_stmt(&stmt.node)
+    }
+
+    pub fn exprs(&mut self, exprs: &[ast::NodeRef<ast::Expr>]) {
+        for expr in exprs {
+            self.expr(expr);
+        }
+    }
+
+    pub fn stmts(&mut self, stmts: &[ast::NodeRef<ast::Stmt>]) {
+        for stmt in stmts {
+            self.stmt(stmt);
+        }
+    }
+}
