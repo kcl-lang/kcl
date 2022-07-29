58360f403fd6c5a079ee55e5eb9d51d8990d9de3
peefy <xpf6677@163.com>
2022-07-29 14:01:40 +0800

    refactor: add macos build and test github actions and refine windows and ubuntu github actions.

--- internal/kclvm_py/scripts/test_grammar.sh
+++ internal/kclvm_py/scripts/test_grammar.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
+# Environment
+if [ -f "/etc/os-release" ]; then
+    source /etc/os-release
+    os=$ID
+else
+    os=$(uname)
+fi
 topdir=$(realpath $(dirname $0)/../../../)
-kclvm_install_dir="$topdir/_build/dist/$os/kclvm"
 kclvm_source_dir="$topdir"
 
-echo PATH=$PATH:$kclvm_source_dir/_build/dist/ubuntu/kclvm/bin >> ~/.bash_profile
+echo PATH=$PATH:$kclvm_source_dir/_build/dist/$os/kclvm/bin >> ~/.bash_profile
 source ~/.bash_profile
+
+export PATH=$PATH:$topdir/_build/dist/$os/kclvm/bin
 
 # Grammar test
 cd $kclvm_source_dir/test/grammar
