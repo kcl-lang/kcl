71586b4798f49bf72fc94184bb4e272f5a31ba36
peefy <xpf6677@163.com>
2022-08-04 13:08:13 +0800

    feat: add both JSON and YAML planning.

--- kclvm/runtime/src/value/val_plan.rs
+++ kclvm/runtime/src/value/val_plan.rs
@@ -46,12 +46,16 @@ fn filter_results(key_values: &ValueRef) -> Vec<ValueRef> {
             }
         } else if value.is_dict() {
             let filtered = filter_results(value);
-            let result = results.get_mut(0).unwrap();
-            result.dict_update_key_value(key.as_str(), filtered[0].clone());
-            // if the value has derived 'STANDALONE' instances, extend them
-            if filtered.len() > 1 {
-                for v in &filtered[1..] {
-                    results.push(v.clone());
+            if !results.is_empty() {
+                let result = results.get_mut(0).unwrap();
+                if !filtered.is_empty() {
+                    result.dict_update_key_value(key.as_str(), filtered[0].clone());
+                }
+                // if the value has derived 'STANDALONE' instances, extend them
+                if filtered.len() > 1 {
+                    for v in &filtered[1..] {
+                        results.push(v.clone());
+                    }
                 }
             }
         } else if value.is_list() {
@@ -173,6 +177,22 @@ impl ValueRef {
             .map(|r| r.to_yaml_string())
             .collect::<Vec<String>>();
         results.join("---\n")
+    }
+
+    /// Plan the value to JSON and YAML strings
+    pub fn plan(&self) -> (String, String) {
+        let results = filter_results(self);
+        let yaml_result = results
+            .iter()
+            .map(|r| r.to_yaml_string())
+            .collect::<Vec<String>>()
+            .join("---\n");
+        let mut list_result = ValueRef::list(None);
+        for r in results {
+            list_result.list_append(&r);
+        }
+        let json_result = list_result.to_json_string();
+        (json_result, yaml_result)
     }
 
     fn filter_results(&self) -> ValueRef {
