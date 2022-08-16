e0cf7efff3750d3824b1505c0c5981c5a25067c7
He1pa <18012015693@163.com>
2022-08-16 20:53:14 +0800

    feat:(lint, walker), add lint check in resolver.
    Add Lint check in resolver. Main changes are:
    1. mode.rs: Definition of `Linter`, the entry for lint check
    2. combinedlintpass.rs: `CombinedLintPass` collects all the lints defined in the lints_def.rs
    3. lints_def.rs: Defined the various lints and the corresponding lintpasses implementation
    4. lintpass.rs: Definition of `Lintpass`
    5. lint.rs: Definition of `Lint`
    6. resolver/mode.rs: Call `lint_check_module` when recursively check the ast, and call `lint_check_scope` after check
    
    re KusionStack#109

---
+++ kclvm/sema/src/lint/lints_def.rs
@@ -0,0 +1,171 @@
+use crate::lint::lint::{Lint, LintArray, LintContext};
+use crate::lint::lintpass::LintPass;
+use crate::resolver::scope::Scope;
+use crate::{declare_lint_pass, resolver::scope::ScopeObjectKind};
+use indexmap::IndexSet;
+use kclvm_ast::ast;
+use kclvm_error::{Handler, Level, Message, Position, Style, WarningKind};
+
+/// The 'import_position' lint detects import statements that are not declared at the top of file.
+/// ### Example
+///
+/// ```kcl
+/// schema Person:
+///     name: str
+///
+/// import foo
+///
+/// ```
+/// ### Explanation
+///
+/// According to the KCL code style conventions, import statement are always declared at the top of the file.
+pub static IMPORT_POSITION: &Lint = &Lint {
+    name: stringify!("IMPORT_POSITION"),
+    level: Level::Warning,
+    desc: "Check for importstmt that are not defined at the top of file",
+    code: "W0413",
+    note: Some("Consider moving tihs statement to the top of the file"),
+};
+
+declare_lint_pass!(ImportPosition => [IMPORT_POSITION]);
+
+impl LintPass for ImportPosition {
+    fn check_module(&mut self, handler: &mut Handler, ctx: &mut LintContext, module: &ast::Module) {
+        let mut first_non_importstmt = std::u64::MAX;
+        for stmt in &module.body {
+            match &stmt.node {
+                ast::Stmt::Import(_import_stmt) => {}
+                _ => {
+                    if stmt.line < first_non_importstmt {
+                        first_non_importstmt = stmt.line
+                    }
+                }
+            }
+        }
+        for stmt in &module.body {
+            if let ast::Stmt::Import(_import_stmt) = &stmt.node {
+                if stmt.line > first_non_importstmt {
+                    handler.add_warning(
+                        WarningKind::ImportPositionWarning,
+                        &[Message {
+                            pos: Position {
+                                filename: ctx.filename.clone(),
+                                line: stmt.line,
+                                column: None,
+                            },
+                            style: Style::Line,
+                            message: format!(
+                                "Importstmt should be placed at the top of the module"
+                            ),
+                            note: Some(
+                                "Consider moving tihs statement to the top of the file".to_string(),
+                            ),
+                        }],
+                    );
+                }
+            }
+        }
+    }
+}
+
+/// The 'unused_import' lint detects import statements that are declared but not used.
+///
+/// ### Example
+///
+/// ```kcl
+/// import foo
+///
+/// schema Person:
+///     name: str
+///
+/// ```
+/// ### Explanation
+///
+/// Useless imports can affect the speed of compilation. It is necessary to remove useless imports from the kcl code.
+pub static UNUSED_IMPORT: &Lint = &Lint {
+    name: stringify!("UNUSED_IMPORT"),
+    level: Level::Warning,
+    desc: "Check for unused importstmt",
+    code: "W0411",
+    note: Some("Consider removing this statement"),
+};
+
+declare_lint_pass!(UnusedImport => [UNUSED_IMPORT]);
+
+impl LintPass for UnusedImport {
+    fn check_scope(&mut self, handler: &mut Handler, _ctx: &mut LintContext, scope: &Scope) {
+        let scope_objs = &scope.elems;
+        for (_, scope_obj) in scope_objs {
+            let scope_obj = scope_obj.borrow();
+            if scope_obj.kind == ScopeObjectKind::Module && scope_obj.used == false {
+                handler.add_warning(
+                    WarningKind::UnusedImportWarning,
+                    &[Message {
+                        pos: Position {
+                            filename: scope_obj.start.filename.clone(),
+                            line: scope_obj.start.line,
+                            column: None,
+                        },
+                        style: Style::Line,
+                        message: format!("Module '{}' imported but unused", scope_obj.name),
+                        note: Some("Consider removing this statement".to_string()),
+                    }],
+                );
+            }
+        }
+    }
+}
+
+/// The 'reimport' lint detects deplicate import statement
+/// ### Example
+///
+/// ```kcl
+/// import foo
+/// import foo
+///
+/// schema Person:
+///     name: str
+///
+/// ```
+/// ### Explanation
+///
+/// The import statement should be declared only once
+pub static REIMPORT: &Lint = &Lint {
+    name: stringify!("REIMPORT"),
+    level: Level::Warning,
+    desc: "Check for deplicate importstmt",
+    code: "W0404",
+    note: Some("Consider removing this statement"),
+};
+
+declare_lint_pass!(ReImport => [REIMPORT]);
+
+impl LintPass for ReImport {
+    fn check_module(&mut self, handler: &mut Handler, ctx: &mut LintContext, module: &ast::Module) {
+        let mut import_names = IndexSet::<String>::new();
+        for stmt in &module.body {
+            if let ast::Stmt::Import(import_stmt) = &stmt.node {
+                if import_names.contains(&import_stmt.path) {
+                    handler.add_warning(
+                        WarningKind::ReimportWarning,
+                        &[Message {
+                            pos: Position {
+                                filename: ctx.filename.clone(),
+                                line: stmt.line,
+                                column: None,
+                            },
+                            style: Style::Line,
+                            message: format!(
+                                "Module '{}' is reimported multiple times",
+                                &import_stmt.name
+                            ),
+                            note: Some("Consider removing this statement".to_string()),
+                        }],
+                    );
+                } else {
+                    import_names.insert(import_stmt.path.clone());
+                }
+            }
+        }
+    }
+}
