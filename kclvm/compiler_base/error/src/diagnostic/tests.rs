96d48bb38739faef5960da4ec82fd5f3c6f7f887
zong-zhe <zongzhe1024@163.com>
2022-08-24 16:29:45 +0800

    feat(compiler-base): add error message depends on fluent.
    add error message loader based on fluent0.16.0.
    
    issue #115

--- kclvm/compiler_base/error/src/diagnostic/tests.rs
+++ kclvm/compiler_base/error/src/diagnostic/tests.rs
@@ -75,3 +75,39 @@ mod test_components {
         assert_eq!(result.get(0).unwrap().get(0).unwrap().style, None);
     }
 }
+
+mod test_error_message {
+    use crate::diagnostic::error_message::{
+        ErrorMessage, MessageArgs, MessageIndex, TemplateLoader,
+    };
+
+    #[test]
+    fn test_template_message() {
+        let template_path = "./src/diagnostic/locales/en-US/default.ftl";
+        let no_args = MessageArgs::new();
+        let msg_index = MessageIndex::from("invalid-syntax");
+        let no_sub_msg_index = None;
+        let template_msg = ErrorMessage::new_template_msg(msg_index, no_sub_msg_index, &no_args);
+        let template_loader = TemplateLoader::new_with_template_path(template_path.to_string());
+        let msg_in_line_1 = template_msg.trans_msg_to_str(Some(&template_loader));
+        assert_eq!(msg_in_line_1, "Invalid syntax");
+
+        let mut args = MessageArgs::new();
+        args.set("expected_items", "I am an expected item");
+        let msg_index = MessageIndex::from("invalid-syntax");
+        let sub_msg_index = MessageIndex::from("expected");
+        let template_msg = ErrorMessage::new_template_msg(msg_index, Some(sub_msg_index), &args);
+        let template_loader = TemplateLoader::new_with_template_path(template_path.to_string());
+        let msg_in_line_2 = template_msg.trans_msg_to_str(Some(&template_loader));
+        assert_eq!(
+            msg_in_line_2,
+            "Expected one of `\u{2068}I am an expected item\u{2069}`"
+        );
+    }
+
+    #[test]
+    fn test_str_message() {
+        let str_msg = ErrorMessage::new_str_msg("This is a str msg".to_string());
+        assert_eq!(str_msg.trans_msg_to_str(None), "This is a str msg");
+    }
+}
