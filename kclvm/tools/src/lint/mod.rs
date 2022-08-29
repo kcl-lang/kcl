1eef77e99f9e70e9a061f75b26a6682b691da547
HeiPa <56333845+He1pa@users.noreply.github.com>
2022-08-29 23:47:40 +0800

    Feat: add `kclvm_cli lint` command to kclvm_cli (#156)
    * feat(kclvm_cli), add kclvm_cli lint command
    add USAGE:
        kclvm_cli lint [FLAGS] [OPTIONS] [--] [INPUT]... command to call resolver lint check and skip runtime
    
    * add documents and test

---
+++ kclvm/tools/src/lint/mod.rs
@@ -0,0 +1,74 @@
+use indexmap::IndexSet;
+use kclvm_error::{Diagnostic, Level};
+use kclvm_parser::{load_program, LoadProgramOptions};
+use kclvm_sema::resolver::resolve_program;
+#[cfg(test)]
+mod tests;
+
+/// KCL Lint tools API, check a set of files, skips execute, divides and returns diagnostics into error and warning
+///
+/// # Parameters
+///
+/// `file`: [&str]
+///     The File that need to be check
+///
+/// `opts`: Option<LoadProgramOptions>
+///     The compilation parameters of KCL, same as the compilation process
+///
+/// # Returns
+///
+/// result: (IndexSet<Diagnostic>, IndexSet<Diagnostic>)
+///     Error and warning diagenostics.
+///
+/// # Examples
+///
+/// ```no_run
+/// use kclvm_tools::lint::lint_files;
+/// (error, warning) = lint_files(vec![&test.k], None));
+/// ```
+///
+/// - test.k
+///
+/// ```kcl
+/// import kcl_plugin.hello
+/// schema Person:
+///     age: int
+/// ```
+///
+/// - return
+/// error: []
+/// warning: [
+///    Diagnostic {
+///        level: Warning
+///        messages: [Message {
+///            pos: Position {
+///                filename: test.k,
+///                line: 1,
+///                column: None,
+///            },
+///            style: Style::Line,
+///            message: "Module 'kcl_plugin.hello' imported but unused",
+///            note: Some("Consider removing this statement".to_string()),
+///        }],
+///        code: Some<WarningKind::UnusedImportWarning>,
+///     }
+/// ]
+pub fn lint_files(
+    files: &[&str],
+    opts: Option<LoadProgramOptions>,
+) -> (IndexSet<Diagnostic>, IndexSet<Diagnostic>) {
+    // Parse AST program.
+    let mut program = load_program(&files, opts).unwrap();
+    let scope = resolve_program(&mut program);
+    let (mut errs, mut warnings) = (IndexSet::new(), IndexSet::new());
+    for diag in &scope.diagnostics {
+        if diag.level == Level::Error {
+            errs.insert(diag.clone());
+        } else if diag.level == Level::Warning {
+            warnings.insert(diag.clone());
+        } else {
+            continue;
+        }
+    }
+    (errs, warnings)
+}
---
+++ kclvm/tools/src/lint/tests.rs
@@ -0,0 +1,14 @@
+use super::lint_files;
+
+#[test]
+fn test_lint() {
+    let (_, warnings) = lint_files(&vec!["./src/lint/test_data/lint.k"], None);
+    let msgs = [
+        "Importstmt should be placed at the top of the module",
+        "Module 'a' is reimported multiple times",
+        "Module 'import_test.a' imported but unused",
+    ];
+    for (diag, m) in warnings.iter().zip(msgs.iter()) {
+        assert_eq!(diag.messages[0].message, m.to_string());
+    }
+}
