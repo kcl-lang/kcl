5b56d10e2822b5b5172b8d76837c50056b4411fa
zong-zhe <zongzhe1024@163.com>
2022-09-06 14:29:30 +0800

    add docstring for span.rs

--- kclvm/compiler_base/span/src/span.rs
+++ kclvm/compiler_base/span/src/span.rs
@@ -5,6 +5,14 @@ pub type Span = rustc_span::Span;
 pub type SpanData = rustc_span::SpanData;
 pub const DUMMY_SP: Span = rustc_span::DUMMY_SP;
 
+/// New a `BytePos`
+///
+/// # Examples
+///
+/// ```rust
+/// use compiler_base_span::span::new_byte_pos;
+/// let byte_pos = new_byte_pos(10);
+/// ```
 pub fn new_byte_pos(arg: u32) -> rustc_span::BytePos {
     rustc_span::BytePos(arg)
 }
