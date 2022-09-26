a2f37ca1bf168e6900142d0454edd68203387874
zong-zhe <zongzhe1024@163.com>
2022-09-26 14:40:16 +0800

    add comments and remove useless struct

--- kclvm/tools/src/util/loader.rs
+++ kclvm/tools/src/util/loader.rs
@@ -3,66 +3,49 @@ use std::fs;
 use anyhow::{bail, Context, Result};
 
 pub(crate) trait Loader<T> {
-    fn load_ast(&self) -> Result<T>;
+    fn load(&self) -> Result<T>;
 }
-
-pub(crate) struct LoaderInner {
-    content: String,
-}
-
 pub(crate) enum LoaderKind {
     YAML,
     JSON,
 }
 
-impl LoaderInner {
-    fn new_with_file_path(file_path: &str) -> Result<Self> {
-        let content = fs::read_to_string(file_path)
-            .with_context(|| format!("Failed to Load '{}'", file_path))?;
-
-        Ok(Self { content })
-    }
-
-    fn new_with_str(s: &str) -> Result<Self> {
-        Ok(Self {
-            content: s.to_string(),
-        })
-    }
-
-    fn get_content(&self) -> &str {
-        &self.content
-    }
-}
-
+/// DataLoader for Json or Yaml
+/// If `DataLoader` is constructed using a file path, then `content` is the content of the file.
+/// If `DataLoader` is constructed using a Json/Yaml string, then `content` is the string
 pub(crate) struct DataLoader {
     kind: LoaderKind,
-    inner: LoaderInner,
+    content: String,
 }
 
 impl DataLoader {
+    /// If `DataLoader` is constructed using a file path, then `content` is the content of the file.
     pub(crate) fn new_with_file_path(loader_kind: LoaderKind, file_path: &str) -> Result<Self> {
+        let content = fs::read_to_string(file_path)
+            .with_context(|| format!("Failed to Load '{}'", file_path))?;
+
         Ok(Self {
             kind: loader_kind,
-            inner: LoaderInner::new_with_file_path(file_path)
-                .with_context(|| format!("Failed to Load '{}'", file_path))?,
+            content,
         })
     }
 
-    pub(crate) fn new_with_str(loader_kind: LoaderKind, s: &str) -> Result<Self> {
+    /// If `DataLoader` is constructed using a Json/Yaml string, then `content` is the string
+    pub(crate) fn new_with_str(loader_kind: LoaderKind, content: &str) -> Result<Self> {
         Ok(Self {
             kind: loader_kind,
-            inner: LoaderInner::new_with_str(s)
-                .with_context(|| format!("Failed to Load '{}'", s))?,
+            content: content.to_string(),
         })
     }
 
     pub(crate) fn get_data(&self) -> &str {
-        &self.inner.get_content()
+        &self.content
     }
 }
 
 impl Loader<serde_json::Value> for DataLoader {
-    fn load_ast(&self) -> Result<serde_json::Value> {
+    /// Load data into Json value.
+    fn load(&self) -> Result<serde_json::Value> {
         let v = match self.kind {
             LoaderKind::JSON => serde_json::to_value(&self.get_data())
                 .with_context(|| format!("Failed to String '{}' to Json", self.get_data()))?,
@@ -76,7 +59,8 @@ impl Loader<serde_json::Value> for DataLoader {
 }
 
 impl Loader<serde_yaml::Value> for DataLoader {
-    fn load_ast(&self) -> Result<serde_yaml::Value> {
+    /// Load data into Yaml value.
+    fn load(&self) -> Result<serde_yaml::Value> {
         let v = match self.kind {
             LoaderKind::YAML => serde_yaml::to_value(&self.get_data())
                 .with_context(|| format!("Failed to String '{}' to Yaml", self.get_data()))?,
