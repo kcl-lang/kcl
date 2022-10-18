cd8fa56b1dac8058979c391b6e55687bbf1038bf
zongz <68977949+zong-zhe@users.noreply.github.com>
2022-10-18 11:07:33 +0800

    Feat(kclvm-version): change new checksum for new version. (#244)
    change check sum from "e07ed7af0d9bd1e86a3131714e4bd20c" to "c5bd1f3a5d6db8c676bafddb6e643660".

--- kclvm/version/src/lib.rs
+++ kclvm/version/src/lib.rs
@@ -1,7 +1,7 @@
 // Copyright 2021 The KCL Authors. All rights reserved.
 
 pub const VERSION: &str = "0.4.3";
-pub const CHECK_SUM: &str = "e07ed7af0d9bd1e86a3131714e4bd20c";
+pub const CHECK_SUM: &str = "c5bd1f3a5d6db8c676bafddb6e643660";
 
 pub fn get_full_version() -> String {
     format!("{}-{}", VERSION, CHECK_SUM)
