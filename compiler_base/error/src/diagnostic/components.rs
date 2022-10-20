51b374d02d1ac7514fbf9404e3124b34c3f4b1c1
zongz <68977949+zong-zhe@users.noreply.github.com>
2022-10-20 10:00:03 +0800

    Fix(compiler-base-error): `CodeSnippet` component generates the line … (#249)
    * Fix(compiler-base-error): `CodeSnippet` component generates the line number plus one.
    
    In `CodeSnippet` components, the line number plus one.
    
    issue #115
    
    * add some comments

--- compiler_base/error/src/diagnostic/components.rs
+++ compiler_base/error/src/diagnostic/components.rs
@@ -351,7 +351,10 @@ impl Component<DiagnosticStyle> for CodeSnippet {
                     )) {
                     Some(sf) => {
                         for line in affected_lines.lines {
-                            let line_index = line.line_index.to_string();
+                            // The line number shown in diagnostic should begin from 1.
+                            // The `line.line_index` get from `SourceMap` begin from 0.
+                            // So, the line number shown in diagnostic should be equal to line.line_index + 1.
+                            let line_index = (line.line_index + 1).to_string();
                             let indent = line_index.len() + 1;
                             IndentWithPrefix::new(line_index, indent, Some(DiagnosticStyle::Url))
                                 .format(sb, errs);
