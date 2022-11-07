4077a80d7679b7d5f338fb1383c266077eec79e7
Peefy <xpf6677@163.com>
2022-11-07 20:45:31 +0800

    chore: bump kclvm version to 0.4.4 (#284)

--- kclvm/version/src/lib.rs
+++ kclvm/version/src/lib.rs
@@ -1,7 +1,7 @@
 // Copyright 2021 The KCL Authors. All rights reserved.
 
-pub const VERSION: &str = "0.4.3";
-pub const CHECK_SUM: &str = "c5bd1f3a5d6db8c676bafddb6e643660";
+pub const VERSION: &str = "0.4.4";
+pub const CHECK_SUM: &str = "9e3303edaba484df6004620bf7b28b98";
 
 pub fn get_full_version() -> String {
     format!("{}-{}", VERSION, CHECK_SUM)
