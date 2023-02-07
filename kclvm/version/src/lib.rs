edd89e696257708b3021ba8df5459cddbc48e1dc
Peefy <xpf6677@163.com>
2023-02-07 19:13:53 +0800

    feat: support config entry mutual reference. (#395)
    * chore: bump version to 0.4.5
    
    * feat: support config entry mutual reference.

--- kclvm/version/src/lib.rs
+++ kclvm/version/src/lib.rs
@@ -1,6 +1,6 @@
 // Copyright 2021 The KCL Authors. All rights reserved.
 
-pub const VERSION: &str = "0.4.4";
+pub const VERSION: &str = "0.4.5";
 pub const CHECK_SUM: &str = "c5339e572207211e46477825e8aca903";
 
 pub fn get_full_version() -> String {
