40579dcc1c43e7ad82e798c83803d484d6ea26f8
never <chenxiangfei.cxf@antgroup.com>
2023-04-10 14:50:23 +0800

    fix : set exitcode in kclvm_cli

--- kclvm_cli/src/main.rs
+++ kclvm_cli/src/main.rs
@@ -1,13 +1,17 @@
 //! The `kclvm` command-line interface.
 
-use std::ffi::{c_char, c_int, CString};
+use std::{
+    ffi::{c_char, c_int, CString},
+    process::ExitCode,
+};
 
 #[link(name = "kclvm_cli_cdylib")]
+#[allow(improper_ctypes)]
 extern "C" {
-    fn kclvm_cli_main(argc: c_int, argv: *const *const c_char) -> *mut c_char;
+    fn kclvm_cli_main(argc: c_int, argv: *const *const c_char) -> *mut ExitCode;
 }
 
-fn main() {
+fn main() -> ExitCode {
     // create a vector of zero terminated strings
     let args = std::env::args()
         .map(|arg| CString::new(arg).unwrap())
@@ -19,10 +23,8 @@ fn main() {
         .collect::<Vec<*const c_char>>();
     unsafe {
         // pass the pointer of the vector's internal buffer to a C function
-        let result = CString::from_raw(kclvm_cli_main(c_args.len() as c_int, c_args.as_ptr()));
-        let result_str = result.to_str().unwrap();
-        if !result_str.is_empty() {
-            println!("{}", result_str);
-        }
+        let result = kclvm_cli_main(c_args.len() as c_int, c_args.as_ptr());
+        let result = Box::from_raw(result);
+        *result
     }
 }
