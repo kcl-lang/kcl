03911df0dbbd83200dcba0b548a4fa9cd3175731
He1pa <56333845+He1pa@users.noreply.github.com>
2023-08-24 20:12:52 +0800

    feat: lsp range fmt  (#682)
    feat: lsp range fmt. Support range format in lsp

--- kclvm/tools/src/LSP/src/formatting.rs
+++ kclvm/tools/src/LSP/src/formatting.rs
@@ -1,9 +1,10 @@
 use kclvm_tools::format::{format_source, FormatOptions};
 use lsp_types::{Position, Range, TextEdit};
 
-pub(crate) fn format_single_file(
+pub(crate) fn format(
     file: String,
     src: String,
+    range: Option<Range>,
 ) -> anyhow::Result<Option<Vec<TextEdit>>> {
     let (source, is_formatted) = format_source(
         &file,
@@ -13,10 +14,13 @@ pub(crate) fn format_single_file(
             ..Default::default()
         },
     )
-    .map_err(|err| anyhow::anyhow!("Formmatting failed: {}", err))?;
+    .map_err(|err| anyhow::anyhow!("Formatting failed: {}", err))?;
     if is_formatted {
         Ok(Some(vec![TextEdit {
-            range: Range::new(Position::new(0, 0), Position::new(u32::MAX, u32::MAX)),
+            range: range.unwrap_or(Range::new(
+                Position::new(0, 0),
+                Position::new(u32::MAX, u32::MAX),
+            )),
             new_text: source,
         }]))
     } else {
