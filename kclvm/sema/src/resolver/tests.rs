6ecaa6ccf14d4460f4525b48d0c92f35682c5515
zongz <68977949+zong-zhe@users.noreply.github.com>
2023-10-30 17:07:56 +0800

    Fix: fix type checkout missing when assigning a dict to a schema (#815)
    fix: fix type checkout missing when assigning a dict to a schema
    
    Signed-off-by: zongz <zongzhe1024@163.com>

--- kclvm/sema/src/resolver/tests.rs
+++ kclvm/sema/src/resolver/tests.rs
@@ -595,3 +595,24 @@ fn test_resolve_lambda_assignment_diagnostic() {
         "expected function (int, int) -> int, got function (int, int) -> str"
     );
 }
+
+#[test]
+fn test_ty_check_in_dict_assign_to_schema() {
+    let sess = Arc::new(ParseSession::default());
+    let mut program = load_program(
+        sess.clone(),
+        &["./src/resolver/test_data/attr_ty_check.k"],
+        None,
+    )
+    .unwrap();
+    let scope = resolve_program(&mut program);
+    assert_eq!(scope.handler.diagnostics.len(), 1);
+    let diag = &scope.handler.diagnostics[0];
+    assert_eq!(diag.code, Some(DiagnosticId::Error(ErrorKind::TypeError)));
+    assert_eq!(diag.messages.len(), 2);
+    assert_eq!(diag.messages[0].message, "expected int, got str(1)");
+    assert_eq!(
+        diag.messages[1].message,
+        "variable is defined here, its type is int, but got str(1)"
+    );
+}
