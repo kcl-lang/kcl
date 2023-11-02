65d23cb623ebdbf31f6c001724bf045a691790e3
jakezhu9 <jakezhu9@gmail.com>
2023-11-02 21:51:45 +0800

    fix: use Arc in sema benchmark
    Signed-off-by: jakezhu9 <jakezhu9@gmail.com>

--- kclvm/sema/benches/my_benchmark.rs
+++ kclvm/sema/benches/my_benchmark.rs
@@ -1,19 +1,19 @@
-use std::rc::Rc;
-
 use criterion::{criterion_group, criterion_main, Criterion};
 use kclvm_sema::ty::*;
+
+use std::sync::Arc;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("sup", |b| {
         b.iter(|| {
             let types = vec![
-                Rc::new(Type::int_lit(1)),
-                Rc::new(Type::INT),
-                Rc::new(Type::union(&[
-                    Rc::new(Type::STR),
-                    Rc::new(Type::dict(Rc::new(Type::STR), Rc::new(Type::STR))),
+                Arc::new(Type::int_lit(1)),
+                Arc::new(Type::INT),
+                Arc::new(Type::union(&[
+                    Arc::new(Type::STR),
+                    Arc::new(Type::dict(Arc::new(Type::STR), Arc::new(Type::STR))),
                 ])),
-                Rc::new(Type::dict(Rc::new(Type::ANY), Rc::new(Type::ANY))),
+                Arc::new(Type::dict(Arc::new(Type::ANY), Arc::new(Type::ANY))),
             ];
             sup(&types);
         })
