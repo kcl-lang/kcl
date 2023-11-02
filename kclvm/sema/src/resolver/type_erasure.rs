9d32495c42d2e1e22d7b0d6829aa61c463af9061
zongz <zongzhe1024@163.com>
2023-11-02 16:39:45 +0800

    fix: add constant for 'function'
    Signed-off-by: zongz <zongzhe1024@163.com>

--- kclvm/sema/src/resolver/type_erasure.rs
+++ kclvm/sema/src/resolver/type_erasure.rs
@@ -3,6 +3,7 @@ use kclvm_ast::walker::MutSelfMutWalker;
 
 #[derive(Default)]
 struct TypeErasureTransformer;
+const FUNCTION: &str = "function";
 
 impl<'ctx> MutSelfMutWalker<'ctx> for TypeErasureTransformer {
     fn walk_schema_stmt(&mut self, schema_stmt: &'ctx mut ast::SchemaStmt) {
@@ -10,7 +11,7 @@ impl<'ctx> MutSelfMutWalker<'ctx> for TypeErasureTransformer {
             if let kclvm_ast::ast::Type::Function(_) =
                 &mut schema_index_signature.node.value_ty.node
             {
-                schema_index_signature.node.value_type.node = "function".to_string();
+                schema_index_signature.node.value_type.node = FUNCTION.to_string();
             }
         }
         for item in schema_stmt.body.iter_mut() {
@@ -22,21 +23,21 @@ impl<'ctx> MutSelfMutWalker<'ctx> for TypeErasureTransformer {
 
     fn walk_schema_attr(&mut self, schema_attr: &'ctx mut ast::SchemaAttr) {
         if let kclvm_ast::ast::Type::Function(_) = schema_attr.ty.as_ref().node {
-            schema_attr.type_str.node = "function".to_string();
+            schema_attr.type_str.node = FUNCTION.to_string();
         }
     }
     fn walk_assign_stmt(&mut self, assign_stmt: &'ctx mut ast::AssignStmt) {
         if let Some(ty) = &mut assign_stmt.ty {
             if let kclvm_ast::ast::Type::Function(_) = ty.as_ref().node {
                 if let Some(ty_anno) = &mut assign_stmt.type_annotation {
-                    ty_anno.node = "function".to_string();
+                    ty_anno.node = FUNCTION.to_string();
                 }
             }
         }
     }
     fn walk_type_alias_stmt(&mut self, type_alias_stmt: &'ctx mut ast::TypeAliasStmt) {
         if let kclvm_ast::ast::Type::Function(_) = type_alias_stmt.ty.as_ref().node {
-            type_alias_stmt.type_value.node = "function".to_string();
+            type_alias_stmt.type_value.node = FUNCTION.to_string();
         }
     }
     fn walk_arguments(&mut self, arguments: &'ctx mut ast::Arguments) {
@@ -48,7 +49,7 @@ impl<'ctx> MutSelfMutWalker<'ctx> for TypeErasureTransformer {
             if let Some(ty) = ty {
                 if let kclvm_ast::ast::Type::Function(_) = ty.as_ref().node {
                     if let Some(ty_anno) = ty_anno {
-                        ty_anno.node = "function".to_string();
+                        ty_anno.node = FUNCTION.to_string();
                     }
                 }
             }
