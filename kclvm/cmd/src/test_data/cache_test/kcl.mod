762125cfdc28c1de45fb92c74d1950ffb935f313
zongz <68977949+zong-zhe@users.noreply.github.com>
2023-11-03 17:07:43 +0800

    fix: fix cache invalidation (#841)
    * fix: fix cache invalidation
    
    Signed-off-by: zongz <zongzhe1024@163.com>
    
    * fix: make fmt
    
    Signed-off-by: zongz <zongzhe1024@163.com>
    
    ---------
    
    Signed-off-by: zongz <zongzhe1024@163.com>

---
+++ kclvm/cmd/src/test_data/cache_test/kcl.mod
@@ -0,0 +1,7 @@
+[package]
+name = "cache_test"
+edition = "0.0.1"
+version = "0.0.1"
+
+[dependencies]
+helloworld = "0.1.0"
--- kclvm/config/src/tests.rs
+++ kclvm/config/src/tests.rs
@@ -1,6 +1,15 @@
-use std::{env, path::PathBuf};
+use kclvm_version as version;
+use std::{
+    collections::HashMap,
+    env,
+    fs::{self, File},
+    path::{Path, PathBuf},
+};
 
-use crate::modfile::{get_vendor_home, KCL_PKG_PATH};
+use crate::{
+    cache::{load_pkg_cache, save_pkg_cache, CacheOption},
+    modfile::{get_vendor_home, KCL_PKG_PATH},
+};
 
 #[test]
 fn test_vendor_home() {
@@ -19,4 +28,47 @@ fn test_vendor_home() {
         .canonicalize()
         .unwrap();
     assert_eq!(get_vendor_home(), kpm_home.display().to_string())
+}
+
+#[test]
+fn test_pkg_cache() {
+    let root = PathBuf::from("./src/testdata/test_cache/")
+        .canonicalize()
+        .unwrap()
+        .display()
+        .to_string();
+    let mut external_pkgs = HashMap::new();
+    external_pkgs.insert(
+        "test_vendor".to_string(),
+        "./src/testdata/test_vendor".to_string(),
+    );
+
+    let lock_path = Path::new(&root)
+        .join(".kclvm/cache")
+        .join(format!("{}-{}", version::VERSION, version::CHECK_SUM))
+        .join("test_target");
+
+    fs::create_dir_all(lock_path.clone()).unwrap();
+    File::create(lock_path.join("test_vendor.lock")).unwrap();
+
+    save_pkg_cache(
+        &root,
+        "test_target",
+        "test_vendor",
+        "test_data",
+        CacheOption::default(),
+        &external_pkgs,
+    )
+    .unwrap();
+
+    assert_eq!(
+        load_pkg_cache(
+            &root,
+            "test_target",
+            "test_vendor",
+            CacheOption::default(),
+            &external_pkgs,
+        ),
+        Some("test_data".to_string())
+    )
 }
