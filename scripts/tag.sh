c4b198d290291bddfb65666e4c9bd7fdedcfdc4c
peefy <xpf6677@163.com>
2023-12-24 20:17:18 +0800

    chore: add tag scripts
    Signed-off-by: peefy <xpf6677@163.com>

---
+++ scripts/tag.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+set -e
+if [ "$1" == "" ]; then
+  echo usage: "$0 VERSION"
+fi
+git tag $1
+git push origin $1
+gh release create $1 --draft --generate-notes --title "$1 Release"
