8642ac240b4f253aea1b2f28ba4244aa52b38f1f
Abhishek Kumar <abhishek22512@gmail.com>
2024-02-21 16:52:51 +0530

    formatted code
    Signed-off-by: Abhishek Kumar <abhishek22512@gmail.com>

--- kclvm/driver/src/kpm_update.rs
+++ kclvm/driver/src/kpm_update.rs
@@ -4,7 +4,6 @@ use std::{path::PathBuf, process::Command};
 
 const MANIFEST_FILE: &str = "kcl.mod";
 
-
 /// Update the KCL module.
 ///
 /// This function calls `kcl mod update` to update the KCL module.
