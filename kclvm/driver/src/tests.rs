93a05fc88687b8ea470a5117c82007c724b3c510
d4v1d03 <a_pandey1@ce.iitr.ac.in>
2024-02-21 15:09:10 +0530

    re
    Signed-off-by: d4v1d03 <a_pandey1@ce.iitr.ac.in>

--- kclvm/driver/src/tests.rs
+++ kclvm/driver/src/tests.rs
@@ -407,10 +407,3 @@ fn test_update_kcl_module_failure() {
     let result = update_kcl_module(manifest_path);
     assert!(result.is_err());
 }
-
-#[test]
-fn test_update_kcl_module_success() {
-    let manifest_path = PathBuf::from("./src/test_data/expand_file_pattern/kcl1");
-    let result = update_kcl_module(manifest_path);
-    assert!(result.is_ok());
-}
