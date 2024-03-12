61a05feb004d6e3c91b76b3c497a815fef3be9de
Peefy <xpf6677@163.com>
2024-03-12 17:20:34 +0800

    refactor: codegen value store and load & add debug info for the entire module (#1126)
    Signed-off-by: peefy <xpf6677@163.com>

--- kclvm/compiler/src/codegen/llvm/utils.rs
+++ kclvm/compiler/src/codegen/llvm/utils.rs
@@ -1,4 +1,4 @@
-// Copyright 2021 The KCL Authors. All rights reserved.
+// Copyright The KCL Authors. All rights reserved.
 
 use kclvm_ast::ast;
 use kclvm_runtime::ApiFunc;
@@ -13,7 +13,7 @@ use super::context::LLVMCodeGenContext;
  */
 
 /// Update runtime context pkgpath
-pub fn update_ctx_pkgpath(gen: &LLVMCodeGenContext, pkgpath: &str) {
+pub(crate) fn update_ctx_pkgpath(gen: &LLVMCodeGenContext, pkgpath: &str) {
     gen.build_void_call(
         &ApiFunc::kclvm_context_set_kcl_pkgpath.name(),
         &[
@@ -24,7 +24,10 @@ pub fn update_ctx_pkgpath(gen: &LLVMCodeGenContext, pkgpath: &str) {
 }
 
 /// Update runtime context filename
-pub fn update_ctx_filename<'ctx, T>(gen: &'ctx LLVMCodeGenContext, node: &'ctx ast::Node<T>) {
+pub(crate) fn update_ctx_filename<'ctx, T>(
+    gen: &'ctx LLVMCodeGenContext,
+    node: &'ctx ast::Node<T>,
+) {
     if !node.filename.is_empty() {
         gen.build_void_call(
             &ApiFunc::kclvm_context_set_kcl_filename.name(),
@@ -37,7 +40,10 @@ pub fn update_ctx_filename<'ctx, T>(gen: &'ctx LLVMCodeGenContext, node: &'ctx a
 }
 
 /// Update runtime context line and column
-pub fn update_ctx_line_col<'ctx, T>(gen: &'ctx LLVMCodeGenContext, node: &'ctx ast::Node<T>) {
+pub(crate) fn update_ctx_line_col<'ctx, T>(
+    gen: &'ctx LLVMCodeGenContext,
+    node: &'ctx ast::Node<T>,
+) {
     let mut current_line = gen.current_line.borrow_mut();
     if node.line != *current_line {
         *current_line = node.line;
@@ -53,7 +59,7 @@ pub fn update_ctx_line_col<'ctx, T>(gen: &'ctx LLVMCodeGenContext, node: &'ctx a
 }
 
 /// Update runtime context line and column
-pub fn update_ctx_current_line(gen: &LLVMCodeGenContext) {
+pub(crate) fn update_ctx_current_line(gen: &LLVMCodeGenContext) {
     let current_line = gen.current_line.borrow_mut();
     gen.build_void_call(
         &ApiFunc::kclvm_context_set_kcl_line_col.name(),
@@ -63,4 +69,10 @@ pub fn update_ctx_current_line(gen: &LLVMCodeGenContext) {
             gen.native_int_value(0),
         ],
     );
+}
+
+/// Reset target vars
+pub(crate) fn reset_target_vars(gen: &LLVMCodeGenContext) {
+    gen.target_vars.borrow_mut().clear();
+    gen.target_vars.borrow_mut().push("".to_string());
 }
