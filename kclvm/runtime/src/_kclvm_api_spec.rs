b8295d42b9cce32bb56594ebbc0ed4ebd8867f39
Shruti Sharma <98698727+shruti2522@users.noreply.github.com>
2024-05-06 11:24:27 +0530

    feat: add file.cp and file.mv functions (#1277)
    * feat: add file.cp and file.mv functions
    
    Signed-off-by: shruti2522 <shruti.apc01@gmail.com>
    
    * fix ci error
    
    Signed-off-by: shruti2522 <shruti.apc01@gmail.com>
    
    * fix ci
    
    Signed-off-by: shruti2522 <shruti.apc01@gmail.com>
    
    ---------
    
    Signed-off-by: shruti2522 <shruti.apc01@gmail.com>

--- kclvm/runtime/src/_kclvm_api_spec.rs
+++ kclvm/runtime/src/_kclvm_api_spec.rs
@@ -1230,6 +1230,14 @@
 // api-spec(c):    kclvm_value_ref_t* kclvm_file_delete(kclvm_context_t* ctx, kclvm_value_ref_t* args, kclvm_value_ref_t* kwargs);
 // api-spec(llvm): declare %kclvm_value_ref_t* @kclvm_file_delete(%kclvm_context_t* %ctx, %kclvm_value_ref_t* %args, %kclvm_value_ref_t* %kwargs);
 
+// api-spec:       kclvm_file_cp
+// api-spec(c):    kclvm_value_ref_t* kclvm_file_cp(kclvm_context_t* ctx, kclvm_value_ref_t* args, kclvm_value_ref_t* kwargs);
+// api-spec(llvm): declare %kclvm_value_ref_t* @kclvm_file_cp(%kclvm_context_t* %ctx, %kclvm_value_ref_t* %args, %kclvm_value_ref_t* %kwargs);
+
+// api-spec:       kclvm_file_mv
+// api-spec(c):    kclvm_value_ref_t* kclvm_file_mv(kclvm_context_t* ctx, kclvm_value_ref_t* args, kclvm_value_ref_t* kwargs);
+// api-spec(llvm): declare %kclvm_value_ref_t* @kclvm_file_mv(%kclvm_context_t* %ctx, %kclvm_value_ref_t* %args, %kclvm_value_ref_t* %kwargs);
+
 // api-spec:       kclvm_template_execute
 // api-spec(c):    kclvm_value_ref_t* kclvm_template_execute(kclvm_context_t* ctx, kclvm_value_ref_t* args, kclvm_value_ref_t* kwargs);
 // api-spec(llvm): declare %kclvm_value_ref_t* @kclvm_template_execute(%kclvm_context_t* %ctx, %kclvm_value_ref_t* %args, %kclvm_value_ref_t* %kwargs);
