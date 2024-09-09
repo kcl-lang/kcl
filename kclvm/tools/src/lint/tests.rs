1f8b5f53f35f5d9e9c106bbf214e34ed1c90f216
peefy <xpf6677@163.com>
2024-09-09 15:31:36 +0800

    refactor: pkg import error message
    Signed-off-by: peefy <xpf6677@163.com>

--- kclvm/tools/src/lint/tests.rs
+++ kclvm/tools/src/lint/tests.rs
@@ -24,8 +24,8 @@ fn test_lint() {
 
     let msgs = [
         "pkgpath abc not found in the program",
-        "try 'kcl mod add abc' to download the package not found",
-        "find more package on 'https://artifacthub.io'",
+        "try 'kcl mod add abc' to download the missing package",
+        "browse more packages at 'https://artifacthub.io'",
         &format!("Cannot find the module abc from {}", path.to_str().unwrap()),
     ];
     assert_eq!(
