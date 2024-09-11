1644547768a7a7895f3155a04345a2d1855d4191
peefy <xpf6677@163.com>
2024-09-11 19:18:59 +0800

    refactor: remove config/list if item newline format
    Signed-off-by: peefy <xpf6677@163.com>

--- kclvm/ast_pretty/src/node.rs
+++ kclvm/ast_pretty/src/node.rs
@@ -513,8 +513,9 @@ impl<'p, 'ctx> MutSelfTypedResultWalker<'ctx> for Printer<'p> {
             |expr| self.expr(expr),
             list_if_item_expr.exprs
         );
-        self.write_indentation(Indentation::DedentWithNewline);
+        self.write_indentation(Indentation::Dedent);
         if let Some(orelse) = &list_if_item_expr.orelse {
+            self.write_newline();
             match &orelse.node {
                 ast::Expr::List(list_expr) => {
                     self.write("else:");
@@ -571,8 +572,9 @@ impl<'p, 'ctx> MutSelfTypedResultWalker<'ctx> for Printer<'p> {
             |entry: &ast::NodeRef<ast::ConfigEntry>| self.write_entry(entry),
             config_if_entry_expr.items
         );
-        self.write_indentation(Indentation::DedentWithNewline);
+        self.write_indentation(Indentation::Dedent);
         if let Some(orelse) = &config_if_entry_expr.orelse {
+            self.write_newline();
             match &orelse.node {
                 ast::Expr::Config(config_expr) => {
                     self.write("else:");
