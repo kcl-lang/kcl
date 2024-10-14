574c86268026e75f648dd548e3731074a3ee419c
he1pa <18012015693@163.com>
2024-10-14 14:32:18 +0800

    fix: fix advanced resolver panic when walk aug asssign stmt
    Signed-off-by: he1pa <18012015693@163.com>

--- kclvm/tools/src/LSP/src/tests.rs
+++ kclvm/tools/src/LSP/src/tests.rs
@@ -2403,3 +2403,10 @@ fn pkg_mod_test() {
         compile_test_file("src/test_data/workspace/pkg_mod_test/test/main.k");
     assert_eq!(diags.iter().filter(|diag| diag.is_error()).count(), 0);
 }
+
+#[test]
+fn aug_assign_without_define() {
+    let (_file, _program, diags, _gs) =
+        compile_test_file("src/test_data/error_code/aug_assign/aug_assign.k");
+    assert_eq!(diags.len(), 1);
+}
