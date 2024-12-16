172c6322b8f6b5bb358179a7fd12e4a03503d116
zongz <zongzhe1024@163.com>
2024-12-16 11:25:40 +0800

    fix: rm metadata update
    Signed-off-by: zongz <zongzhe1024@163.com>

--- kclvm/driver/src/toolchain.rs
+++ kclvm/driver/src/toolchain.rs
@@ -59,7 +59,6 @@ impl<S: AsRef<OsStr> + Send + Sync> Toolchain for CommandToolchain<S> {
         match Command::new(&self.path)
             .arg("mod")
             .arg("metadata")
-            .arg("--update")
             .current_dir(manifest_path)
             .output()
         {
