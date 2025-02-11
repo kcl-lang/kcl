902cc72999a80fcf710d6822098791afa435e569
Siddhi Agrawal <siddhi3agrawal@gmail.com>
2025-02-12 02:52:56 +0530

    feat: Add lint all feature to lint all the KCL code
    Signed-off-by: Siddhi Agrawal <siddhi3agrawal@gmail.com>

--- kclvm/tools/src/lint/mod.rs
+++ kclvm/tools/src/lint/mod.rs
@@ -5,6 +5,8 @@ use kclvm_error::{Diagnostic, Handler};
 use kclvm_parser::{load_program, LoadProgramOptions, ParseSession};
 use kclvm_runtime::PanicInfo;
 use kclvm_sema::resolver::resolve_program_with_opts;
+use std::fs;
+use std::path::{Path};
 #[cfg(test)]
 mod tests;
 
@@ -94,4 +96,35 @@ pub fn lint_files(
         .diagnostics,
     )
     .classification()
+}
+
+fn collect_kcl_files<P: AsRef<Path>>(dir: P) -> Vec<String> {
+    let mut files = Vec::new();
+    if let Ok(entries) = fs::read_dir(dir) {
+        for entry in entries.flatten() {
+            let path = entry.path();
+            if path.is_file() && path.extension().map_or(false, |ext| ext == "k") {
+                files.push(path.to_string_lossy().into_owned());
+            } else if path.is_dir() {
+                files.extend(collect_kcl_files(path));
+            }
+        }
+    }
+    files
+}
+
+pub fn lint_all_files(dir: &str, opts: Option<LoadProgramOptions>) -> (IndexSet<Diagnostic>, IndexSet<Diagnostic>) {
+    let files = collect_kcl_files(dir);
+
+    let mut all_errors = IndexSet::new();
+    let mut all_warnings = IndexSet::new();
+
+    for file in files {
+        let file_refs = vec![file.as_str()];
+        let (errors, warnings) = lint_files(&file_refs, opts.clone());
+        all_errors.extend(errors);
+        all_warnings.extend(warnings);
+    }
+
+    (all_errors, all_warnings)
 }
