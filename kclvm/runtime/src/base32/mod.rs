309b59391758d2e1094366136598b24b3e512e3e
Gitanshu Talwar <91304909+gitatractivo@users.noreply.github.com>
2025-03-24 17:52:02 +0530

    Base32 Module Integration for KCL Standard Library  (#1883)
    feat: add base32 module to KCL standard library
    
    - Added BASE32 constant to STANDARD_SYSTEM_MODULES list
    - Implemented base32 encoding and decoding functions
    - Updated test files to use the correct Base32 encoding/decoding
    - Fixed imports in test files
    - Updated completion test to include base32 module
    - Ensured all tests pass with the new module
    
    Fixes #1860
    
    Signed-off-by: Gitanshu Talwar <gitanshutalwar@gmail.com>
    
    Reverted kcl.mod file
    
    Signed-off-by: Gitanshu Talwar <gitanshutalwar@gmail.com>
    
    - Added base32 in test_data/system_package.k
    
    Signed-off-by: Gitanshu Talwar <gitanshutalwar@gmail.com>
    
    - Changing assertion

---
+++ kclvm/runtime/src/base32/mod.rs
@@ -0,0 +1,44 @@
+//! Copyright The KCL Authors. All rights reserved.
+
+extern crate base32;
+use crate::*;
+use base32::{decode, encode, Alphabet};
+
+#[no_mangle]
+#[runtime_fn]
+pub extern "C-unwind" fn kclvm_base32_encode(
+    ctx: *mut kclvm_context_t,
+    args: *const kclvm_value_ref_t,
+    kwargs: *const kclvm_value_ref_t,
+) -> *const kclvm_value_ref_t {
+    let args = ptr_as_ref(args);
+    let kwargs = ptr_as_ref(kwargs);
+    let ctx = mut_ptr_as_ref(ctx);
+    if let Some(s) = get_call_arg_str(args, kwargs, 0, Some("value")) {
+        let s = encode(Alphabet::RFC4648 { padding: true }, s.as_bytes());
+        return ValueRef::str(s.as_str()).into_raw(ctx);
+    }
+    panic!("encode() missing 1 required positional argument: 'value'");
+}
+
+#[no_mangle]
+#[runtime_fn]
+pub extern "C-unwind" fn kclvm_base32_decode(
+    ctx: *mut kclvm_context_t,
+    args: *const kclvm_value_ref_t,
+    kwargs: *const kclvm_value_ref_t,
+) -> *mut kclvm_value_ref_t {
+    let args = ptr_as_ref(args);
+    let kwargs = ptr_as_ref(kwargs);
+    let ctx = mut_ptr_as_ref(ctx);
+    if let Some(s) = get_call_arg_str(args, kwargs, 0, Some("value")) {
+        if let Some(de_str) = decode(Alphabet::RFC4648 { padding: true }, &s) {
+            if let Ok(s) = std::str::from_utf8(&de_str) {
+                return ValueRef::str(s).into_raw(ctx);
+            }
+        }
+        // Handle decoding errors
+        return ValueRef::none().into_raw(ctx);
+    }
+    panic!("decode() missing 1 required positional argument: 'value'");
+}
